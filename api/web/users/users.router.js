eb04d4361fec8770ff4566be5c6ebf4d301f875f
maxickdrobot <maxdrobott@gmail.com>
2025-05-30 16:07:15 +0300

    Added session, passport, and Google auth

--- api/web/users/users.router.js
+++ api/web/users/users.router.js
@@ -4,15 +4,19 @@ const router = express.Router();
 const usersControllers = require("./users.controller");
 const usersMiddlewares = require("./user.middleware");
 const authMiddlewares = require("../../common/middleware/auth.maiddleware");
+const passport = require("passport");
 
-router.get("/views", usersControllers.renderUsers);
-router.get("/views/:userId", usersControllers.renderUser);
+router.get("/views", [authMiddlewares.checkAuth], usersControllers.renderUsers);
+router.get("/views/:userId", [authMiddlewares.checkAuth], usersControllers.renderUser);
 router.get("/settings/theme", usersControllers.getTheme);
 router.post("/settings/theme", [usersMiddlewares.checkThemeExist], usersControllers.setTheme);
-router.get("/:userId", [authMiddlewares.checkAuth], usersControllers.getUserById);
 
 router.post("/register", [usersMiddlewares.checkUserExistFalse], usersControllers.register);
-router.post("/login", [usersMiddlewares.checkUserExistTrue], usersControllers.login);
-router.post("/logout", usersControllers.logout);
+router.post("/login", [passport.authenticate("local")], usersControllers.login);
+router.get("/logout", usersControllers.logout);
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
+router.get("/google/callback", passport.authenticate("google"), usersControllers.login);
+
+router.get("/:userId", [authMiddlewares.checkAuth], usersControllers.getUserById);
 module.exports = router;
